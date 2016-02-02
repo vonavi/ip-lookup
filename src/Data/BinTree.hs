ac8f1c9dafd71cc059d2ab03d00cbf2df3f00bb0
Vladimir Ivanov <ivvl82@gmail.com>
2016-02-02 10:34:19 +0300

    Change type `Tree' to `BinTree'.

---
+++ src/Data/BinTree.hs
@@ -0,0 +1,43 @@
+module Data.BinTree
+       (
+         BinTree
+       ) where
+
+import Data.Monoid
+import Control.Monad.State
+
+import Data.IpRouter
+
+data Tree a = Tip | Bin (Tree a) a (Tree a) deriving Show
+
+instance Monoid a => Monoid (Tree a) where
+  mempty = Tip
+  Tip `mappend` x = x
+  x `mappend` Tip = x
+  (Bin ll x lr) `mappend` (Bin rl y rr) =
+    Bin (ll `mappend` rl) (x `mappend` y) (lr `mappend` rr)
+
+newtype BinTree = BinTree { getTree :: Tree (Last Int) } deriving Show
+
+instance Monoid BinTree where
+  mempty        = BinTree mempty
+  x `mappend` y = BinTree $ getTree x `mappend` getTree y
+
+fromEntry :: Entry -> BinTree
+fromEntry (Entry p n) = BinTree $ helper . prefixBits $ p
+  where helper :: [Bool] -> Tree (Last Int)
+        helper []     = Bin Tip (Last (Just n)) Tip
+        helper (b:bs) = if b
+                        then Bin Tip (Last Nothing) (helper bs)
+                        else Bin (helper bs) (Last Nothing) Tip
+
+lookupState :: [Bool] -> Tree (Last Int) -> State (Last Int) ()
+lookupState _      Tip         = return ()
+lookupState []     (Bin _ x _) = modify (`mappend` x)
+lookupState (b:bs) (Bin l x r) = do modify (`mappend` x)
+                                    lookupState bs $ if b then r else l
+
+instance IpRouter BinTree where
+  ipInsert e t           = t `mappend` fromEntry e
+  ipLookup a (BinTree t) = getLast $
+                           execState (lookupState (addrBits a) t) (Last Nothing)
