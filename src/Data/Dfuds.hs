f340e6a8a0385200e343113ecaf0cf557f626aed
Vladimir Ivanov <ivvl82@gmail.com>
2016-02-10 17:23:01 +0300

    `Data.Dfuds': more operations implemented.

--- src/Data/Dfuds.hs
+++ src/Data/Dfuds.hs
@@ -5,11 +5,18 @@
 module Data.Dfuds
        (
          parent
+       , firstChild
+       , lastChild
+       , nextSibling
+       , prevSibling
+       , subtreeSize
        , degree
        , child
        ) where
 
 import Data.Monoid
+import Data.Maybe (fromMaybe)
+import Control.Applicative ((<*>))
 
 import Data.Paren
 import Data.OrdTree
@@ -29,10 +36,35 @@ parent n dfuds = do no <- findOpen (pred n) ps
                     Just . succ $ selectClose (rankClose no ps) ps
   where ps = toParens dfuds
 
+firstChild :: Dfuds a => Int -> a -> Maybe Int
+firstChild n = child n 0
+
+lastChild :: Dfuds a => Int -> a -> Maybe Int
+lastChild n dfuds = Just succ <*> findClose n (toParens dfuds)
+
+nextSibling :: Dfuds a => Int -> a -> Maybe Int
+nextSibling n dfuds = do no <- findOpen (pred n) ps
+                         Just succ <*> findClose (pred no) ps
+  where ps = toParens dfuds
+
+prevSibling :: Dfuds a => Int -> a -> Maybe Int
+prevSibling n dfuds = do no <- findOpen (pred n) ps
+                         Just succ <*> findClose (succ no) ps
+  where ps = toParens dfuds
+
+subtreeSize :: Dfuds a => Int -> a -> Int
+subtreeSize n dfuds
+  | n < 0 || n >= length ps = 0
+  | isClose n ps            = 1
+  | otherwise               = succ $ (nc - n) `div` 2
+  where ps = toParens dfuds
+        nc = fromMaybe (length ps) $ do ne <- enclose n ps
+                                        findClose ne ps
+
 degree :: Dfuds a => Int -> a -> Int
 degree n dfuds
-  | isClose n ps = 0
-  | otherwise    = nc - n
+  | not (isOpen n ps) = 0
+  | otherwise         = nc - n
   where ps = toParens dfuds
         nc = selectClose (succ $ rankClose n ps) ps
 
