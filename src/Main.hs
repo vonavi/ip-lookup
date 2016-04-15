8178e7f330f39282478c8f8fdcac747e52108fd8
Vladimir Ivanov <ivvl82@gmail.com>
2016-04-15 09:53:50 +0300

    `src/Main.hs': slight changes.

--- src/Main.hs
+++ src/Main.hs
@@ -3,19 +3,19 @@ module Main where
 import Data.IpRouter
 import RandomPrefixes
 import Data.PatTree
-import qualified Data.PatSst as PT
+import qualified Data.PatSst as PS
 import Data.PatSst (MhPatSst)
-import qualified Data.OrdSst as OT
+import qualified Data.OrdSst as OS
 import Data.OrdSst (MhOrdSstT1, MhOrdSstT2, MhOrdSstT3, MhOrdSstT4)
 
 randomIpRouter :: IpRouter a => Int -> a
 randomIpRouter n = mkTable $ randomEntries (32, 32) [1 .. n]
 
 memUsage :: MhPatSst -> Int
-memUsage = (*) PT.maxPageSize . PT.numOfPages
+memUsage = (*) PS.maxPageSize . PS.numOfPages
 
 fillRatio :: MhPatSst -> Double
-fillRatio t = fromIntegral (PT.fillSize t) / fromIntegral (memUsage t)
+fillRatio t = fromIntegral (PS.fillSize t) / fromIntegral (memUsage t)
 
 putPatTree :: IO ()
 putPatTree = do
@@ -28,10 +28,10 @@ putPatTree = do
 putMhPatSst :: IO ()
 putMhPatSst = do
   putStrLn "Min-height SST for PATRICIA tree"
-  putStrLn . (++) "  Height:          " . show . PT.height $ t
-  putStrLn . (++) "  Number of pages: " . show . PT.numOfPages $ t
+  putStrLn . (++) "  Height:          " . show . PS.height $ t
+  putStrLn . (++) "  Number of pages: " . show . PS.numOfPages $ t
   putStrLn . (++) "  Memory usage:    " . show . memUsage $ t
-  putStrLn . (++) "  Fill size:       " . show . PT.fillSize $ t
+  putStrLn . (++) "  Fill size:       " . show . PS.fillSize $ t
   putStrLn . (++) "  Fill ratio:      " . show . fillRatio $ t
     where n = 1000000
           t = randomIpRouter n :: MhPatSst
@@ -40,24 +40,24 @@ putMhPatSst = do
 putMhOrdSst :: IO ()
 putMhOrdSst = do
   putStrLn "Min-height SST for ordinal tree T1"
-  putStrLn . (++) "  Height:          " . show . OT.height $ t1
-  putStrLn . (++) "  Number of pages: " . show . OT.numOfPages $ t1
-  putStrLn . (++) "  Fill size:       " . show . OT.fillSize $ t1
+  putStrLn . (++) "  Height:          " . show . OS.height $ t1
+  putStrLn . (++) "  Number of pages: " . show . OS.numOfPages $ t1
+  putStrLn . (++) "  Fill size:       " . show . OS.fillSize $ t1
 
   putStrLn "Min-height SST for ordinal tree T2"
-  putStrLn . (++) "  Height:          " . show . OT.height $ t2
-  putStrLn . (++) "  Number of pages: " . show . OT.numOfPages $ t2
-  putStrLn . (++) "  Fill size:       " . show . OT.fillSize $ t2
+  putStrLn . (++) "  Height:          " . show . OS.height $ t2
+  putStrLn . (++) "  Number of pages: " . show . OS.numOfPages $ t2
+  putStrLn . (++) "  Fill size:       " . show . OS.fillSize $ t2
 
   putStrLn "Min-height SST for ordinal tree T3"
-  putStrLn . (++) "  Height:          " . show . OT.height $ t3
-  putStrLn . (++) "  Number of pages: " . show . OT.numOfPages $ t3
-  putStrLn . (++) "  Fill size:       " . show . OT.fillSize $ t3
+  putStrLn . (++) "  Height:          " . show . OS.height $ t3
+  putStrLn . (++) "  Number of pages: " . show . OS.numOfPages $ t3
+  putStrLn . (++) "  Fill size:       " . show . OS.fillSize $ t3
 
   putStrLn "Min-height SST for ordinal tree T4"
-  putStrLn . (++) "  Height:          " . show . OT.height $ t4
-  putStrLn . (++) "  Number of pages: " . show . OT.numOfPages $ t4
-  putStrLn . (++) "  Fill size:       " . show . OT.fillSize $ t4
+  putStrLn . (++) "  Height:          " . show . OS.height $ t4
+  putStrLn . (++) "  Number of pages: " . show . OS.numOfPages $ t4
+  putStrLn . (++) "  Fill size:       " . show . OS.fillSize $ t4
   where n  = 1000000
         t1 = randomIpRouter n :: MhOrdSstT1
         t2 = randomIpRouter n :: MhOrdSstT2
