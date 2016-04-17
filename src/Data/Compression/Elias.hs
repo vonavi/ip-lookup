8c66aa7420d0cc098cece7467642d2bc75e17398
Vladimir Ivanov <ivvl82@gmail.com>
2016-04-17 17:59:32 +0300

    Elias delta coding added.

--- src/Data/Compression/Elias.hs
+++ src/Data/Compression/Elias.hs
@@ -2,8 +2,11 @@ module Data.Compression.Elias
        (
          encodeEliasGamma
        , decodeEliasGamma
+       , encodeEliasDelta
+       , decodeEliasDelta
        ) where
 
+import           Data.Bits
 import           Data.Monoid ((<>))
 
 import           Data.Bitmap
@@ -16,3 +19,14 @@ encodeEliasGamma x | x <= 0    = error "Not positive number"
 decodeEliasGamma :: Bitmap -> Int
 decodeEliasGamma bmp = toInt . takeBits (succ s) . dropBits s $ bmp
   where s = leadingZeros bmp
+
+encodeEliasDelta :: Int -> Bitmap
+encodeEliasDelta x | x <= 0    = error "Not positive number"
+                   | otherwise = encodeEliasGamma (size bmp) <> dropBits 1 bmp
+  where bmp = fromInt x
+
+decodeEliasDelta :: Bitmap -> Int
+decodeEliasDelta bmp = 1 `shiftL` n + x
+  where l = leadingZeros bmp
+        n = pred . toInt . takeBits (2 * l + 1) $ bmp
+        x = toInt . takeBits n . dropBits (2 * l + 1) $ bmp
