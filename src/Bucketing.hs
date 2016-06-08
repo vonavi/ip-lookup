5fce9a109a08ddc80847e97fe3c06e470e77ed37
Vladimir Ivanov <ivvl82@gmail.com>
2016-06-08 16:28:18 +0300

    Fix module `Bucketing' to work with typeclass `Data.Partition'.

--- src/Bucketing.hs
+++ src/Bucketing.hs
@@ -4,15 +4,15 @@ module Bucketing
        ) where
 
 import           Data.Bits
-import           Data.List      (sortBy, unfoldr)
-import           Data.Ord       (comparing)
+import           Data.List          (sortBy, unfoldr)
+import           Data.Ord           (comparing)
 import           Data.Word
 
 import           Data.IpRouter
-import           Data.PatSst    (MhPatSst, PatSst)
-import qualified Data.PatSst    as PS
+import           Data.PaCoPartition
+import           Data.Partition     (Partition)
+import qualified Data.Partition     as P
 import           RandomPrefixes
-
 
 splitEntryAt :: Int -> Entry -> (Word32, Entry)
 splitEntryAt i (Entry p n) = (a `shiftR` (32 - i), Entry p' n)
@@ -29,33 +29,33 @@ groupEntries i es = unfoldr mkGroups gs
         mkGroups xs@((w, _) : _) = Just (span p xs)
           where p (w', _) = w' == w
 
-data PatSstProps = PatSstProps { height    :: Int
-                               , memUsage  :: Int
-                               , fillRatio :: Double
-                               }
-                 deriving Show
+data PrtnProps = PrtnProps { height    :: Int
+                           , memUsage  :: Int
+                           , fillRatio :: Double
+                           }
+               deriving Show
 
-getPatSstProps :: PatSst a => a -> PatSstProps
-getPatSstProps t = PatSstProps { height    = PS.height t
-                               , memUsage  = PS.memUsage t
-                               , fillRatio = PS.fillRatio t
-                               }
+getPrtnProps :: Partition a => a -> PrtnProps
+getPrtnProps t = PrtnProps { height    = P.height t
+                           , memUsage  = P.memUsage t
+                           , fillRatio = P.fillRatio t
+                           }
 
-accPatSstProps :: [PatSstProps] -> PatSstProps
-accPatSstProps ps = PatSstProps { height    = maximum . map height $ ps
-                                , memUsage  = sum . map memUsage $ ps
-                                , fillRatio = (sum . map fillRatio $ ps) /
-                                              fromIntegral (length ps)
-                                }
+accPrtnProps :: [PrtnProps] -> PrtnProps
+accPrtnProps ps = PrtnProps { height    = maximum . map height $ ps
+                            , memUsage  = sum . map memUsage $ ps
+                            , fillRatio = (sum . map fillRatio $ ps) /
+                                          fromIntegral (length ps)
+                            }
 
 putBuckets :: Int -> IO ()
 putBuckets b = do
-  putStrLn "SST for PATRICIA tree with bucketing"
+  putStrLn "Partition of path-compressed tree with bucketing"
   putStrLn . (++) "  Bucket size:  " . show $ b
   putStrLn . (++) "  Height:       " . show . height $ prop
   putStrLn . (++) "  Memory usage: " . show . memUsage $ prop
   putStrLn . (++) "  Fill ratio:   " . show . fillRatio $ prop
   where n    = 1000000
         ess  = map (map snd) . groupEntries b $ randomEntries (32, 32) [1 .. n]
-        prop = accPatSstProps .
-               map (\es -> getPatSstProps (mkTable es :: MhPatSst)) $ ess
+        prop = accPrtnProps .
+               map (\es -> getPrtnProps (mkTable es :: MhPaCoPrtn)) $ ess
