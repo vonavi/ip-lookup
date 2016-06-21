ad7a5e8a13a14210a985a7422f3d78ae7badaf3e
Vladimir Ivanov <ivvl82@gmail.com>
2016-06-21 13:25:05 +0300

    Run experiments with `Data/PaCo2Partition'.

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import           System.Directory
 
 import           Data.IpRouter
-import           Data.PaCoPartition
+import           Data.PaCo2Partition
 
 main :: IO ()
 main = do
@@ -11,7 +11,7 @@ main = do
   input <- readFile $ pwd ++ "/1.route"
   let (_ : _ : prefixLines) = lines input
       es                    = map (getEntry . words) prefixLines
-  putPaCoPrtn (mkTable es :: MhPaCoPrtn)
+  putPaCo2Prtn (mkTable es :: MhPaCo2Prtn)
   where getEntry (aStr : mStr : _ : _ : nStr : _) = Entry (Prefix a m) n
           where a = strToAddr aStr
                 m = strToMask $ '/' : mStr
