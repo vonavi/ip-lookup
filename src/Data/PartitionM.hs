cfce3335b8cd4da38440c3abf69937f21a2ca630
Vladimir Ivanov <ivvl82@gmail.com>
2016-09-26 16:38:05 +0300

    `Data.PartitionM': s/Leaf/Tip/g.

--- src/Data/PartitionM.hs
+++ src/Data/PartitionM.hs
@@ -20,14 +20,14 @@ data Node a = Node { zipper :: a
                    , height :: Int
                    }
             deriving (Show, Eq)
-data Tree a = Leaf (Maybe a)
+data Tree a = Tip (Maybe a)
             | Bin (Maybe a) (Tree a) (Tree a)
             deriving Eq
 type MemTree a = Tree (Node a)
 
 instance Show a => Show (Tree a) where
   show = head . nodeList
-    where nodeList (Leaf _)           = []
+    where nodeList (Tip _)            = []
           nodeList (Bin Nothing l r)  = nodeList l ++ nodeList r
           nodeList (Bin (Just x) l r) = ["Branch (" ++ show x ++ ", [" ++
                                          accStr (nodeList l ++ nodeList r) ++
@@ -36,26 +36,26 @@ instance Show a => Show (Tree a) where
                   accStr xs = drop 2 . concatMap (", " ++) $ xs
 
 instance Foldable Tree where
-  foldMap _ (Leaf _)    = mempty
+  foldMap _ (Tip _)     = mempty
   foldMap f (Bin x l r) = fromMaybe mempty (f <$> x) <>
                           foldMap f l <> foldMap f r
 
 
 rootZipper :: Zipper a => MemTree a -> a
-rootZipper t = case t of Leaf x    -> toZipper x
+rootZipper t = case t of Tip x     -> toZipper x
                          Bin x _ _ -> toZipper x
   where toZipper = fromMaybe (error "No zipper") . (zipper <$>)
 
 rootHeight :: MemTree a -> Int
-rootHeight (Leaf _)    = 0
+rootHeight (Tip _)     = 0
 rootHeight (Bin x _ _) = fromMaybe 0 $ height <$> x
 
 updatePointer :: Node a -> MemTree a -> MemTree a
-updatePointer x (Leaf y)    = Leaf (y <|> Just x)
+updatePointer x (Tip y)     = Tip (y <|> Just x)
 updatePointer x (Bin y l r) = Bin (y <|> Just x) l r
 
 removePointer :: MemTree a -> MemTree a
-removePointer (Leaf _)    = Leaf Nothing
+removePointer (Tip _)     = Tip Nothing
 removePointer (Bin _ l r) = Bin Nothing l r
 
 minPageSize :: Int
@@ -70,8 +70,8 @@ pageSize x = size (zipper x) + 18
 
 
 pruneZipper :: Zipper a => MemTree a -> a -> a
-pruneZipper (Leaf _) = id
-pruneZipper _        = delete
+pruneZipper (Tip _) = id
+pruneZipper _       = delete
 
 mergeBoth :: Zipper a => a -> MemTree a -> MemTree a -> MemTree a
 mergeBoth z l r = Bin (Just x) (removePointer l) (removePointer r)
@@ -114,9 +114,9 @@ minHeightMerge z l r
                   | otherwise  = mergeRight
 
 prtnBuild :: Zipper a => a -> MemTree a
-prtnBuild z | isLeaf z  = Leaf . Just $ Node { zipper = z
-                                             , height = 0
-                                             }
+prtnBuild z | isLeaf z  = Tip . Just $ Node { zipper = z
+                                            , height = 0
+                                            }
             | otherwise = minHeightMerge z' l r
   where zl  = goLeft z
         l   = prtnBuild zl
@@ -127,7 +127,7 @@ prtnBuild z | isLeaf z  = Leaf . Just $ Node { zipper = z
         z'  = goUp zr'
 
 prtnInsert :: Zipper a => a -> MemTree a -> MemTree a
-prtnInsert z (Leaf _)           = prtnBuild z
+prtnInsert z (Tip _)            = prtnBuild z
 prtnInsert z t | isLeaf z       = t
 prtnInsert z (Bin (Just x) l r) = minHeightMerge (setLabel s z') l' r'
   where h   = height x
@@ -145,11 +145,11 @@ prtnInsert _ (Bin Nothing _ _)  = error "No pointer"
 
 delEmptyPage :: Zipper a => MemTree a -> MemTree a
 delEmptyPage t
-  | (Bin (Just x) Leaf{} Leaf{}) <- t
+  | (Bin (Just x) Tip{} Tip{}) <- t
   , isNothing . getLabel . zipper $ x
-    = Leaf . Just $ Node { zipper = delete . zipper $ x
-                         , height = 0
-                         }
+    = Tip . Just $ Node { zipper = delete . zipper $ x
+                        , height = 0
+                        }
   | otherwise
     = t
 
@@ -161,7 +161,7 @@ delLabel z1 z2
   | otherwise              = getLabel z1
 
 prtnDelete :: Zipper a => a -> MemTree a -> MemTree a
-prtnDelete _ t@(Leaf _)         = t
+prtnDelete _ t@(Tip _)          = t
 prtnDelete z t | isLeaf z       = t
 prtnDelete z (Bin (Just x) l r) = delEmptyPage $
                                   minHeightMerge (setLabel s z') l' r'
@@ -180,7 +180,7 @@ prtnDelete _ (Bin Nothing _ _)  = error "No pointer"
 
 lookupState :: Zipper a => Address -> MemTree a -> State (Maybe Int) ()
 lookupState (Address a) = helper 31
-  where helper _ (Leaf _)           = return ()
+  where helper _ (Tip _)            = return ()
         helper n (Bin (Just x) l r) = do
           modify (getLabel z <|>)
           if a `testBit` n
