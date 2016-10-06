a7622adca62546c61d08316f2188f344410c5eb4
Vladimir Ivanov <ivvl82@gmail.com>
2016-10-06 15:38:24 +0300

    `Data.Entry': improve the reading of IPv6 address.

--- src/Data/Entry.hs
+++ src/Data/Entry.hs
@@ -2,6 +2,8 @@
 
 module Data.Entry
   (
+    Ipv4Address
+  , Ipv6Address
   ) where
 
 import           Control.Arrow       (first, second)
@@ -9,7 +11,7 @@ import           Control.Monad.State
 import           Data.Bits
 import           Data.Char
 import           Data.List           (intercalate)
-import           Data.List.Split     (splitOn)
+import           Data.List.Split
 import           Data.Word
 
 splitBitsAt :: (Num a, Bits a) => Int -> a -> (a, a)
@@ -49,19 +51,23 @@ instance Show HexDigit where
 type Hex = [HexDigit]
 
 instance {-# OVERLAPPING #-} Read Hex where
-  readsPrec _ s = [ (map (read . (:[])) . dropWhile ('0' ==) $ hex, other) ]
+  readsPrec _ s
+    | null digits = []
+    | null hex    = [ ([HexDigit 0], other) ]
+    | otherwise   = [ (map (read . (:[])) hex, other) ]
     where (digits, str) = span isHexDigit s
-          (hex, other)  = second (++ str) . splitAt 4 $ digits
+          (hex, other)  = first (dropWhile ('0' ==)) . second (++ str)
+                          . splitAt 4 $ digits
 
 instance {-# OVERLAPPING #-} Show Hex where
-  show [] = "0"
-  show xs = map hexDigitToChar xs
+  show = map hexDigitToChar
 
 hexToInt :: Hex -> Int
 hexToInt xs = sum . zipWith (flip shiftL) offsets . map unHexDigit $ xs
   where offsets = drop (4 - length xs) [12, 8, 4, 0]
 
 intToHex :: Int -> Hex
+intToHex 0 = [HexDigit 0]
 intToHex x = map HexDigit . dropWhile (0 ==)
              . (`evalState` x) . mapM (state . splitBitsAt) $ [12, 8, 4, 0]
 
@@ -74,11 +80,16 @@ fromHexList = Ipv6Address . sum . zipWith (flip shiftL) offsets
 
 instance Read Ipv6Address where
   readsPrec _ s
-    | length xs /= 8 = []
-    | otherwise      = map (first fromHexList) . (readList :: ReadS [Hex])
-                       $ "[" ++ intercalate "," xs ++ "]" ++ other
-    where (addr, other) = span (\c -> isHexDigit c || ':' == c) s
-          xs            = splitOn ":" addr
+    | lenx > 8                        = []
+    | (lenx /= 8) /= ("::" `elem` xs) = []
+    | otherwise                       =
+      map (first fromHexList) . (readList :: ReadS [Hex])
+      . ("[" ++) . (++ "]" ++ other) . intercalate "," . map addZeros $ xs
+    where (ip, other) = span (\c -> isHexDigit c || ':' == c) s
+          xs          = filter (":" /=) . split (dropBlanks $ oneOf ":") $ ip
+          lenx        = length xs
+          addZeros "::" = intercalate "," $ replicate (9 - length xs) "0"
+          addZeros x    = x
 
 instance Show Ipv6Address where
   show (Ipv6Address x) = intercalate ":" . map showHex $ offsets
