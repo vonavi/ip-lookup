c48f22fe143a2fd08dd37c30475d69437026962f
Vladimir Ivanov <ivvl82@gmail.com>
2016-10-20 15:23:09 +0300

    `EntrySpec': let `Address' specifications follow RFC 5952.

--- test/EntrySpec.hs
+++ test/EntrySpec.hs
@@ -15,17 +15,18 @@ ipv4AddressSpec :: Spec
 ipv4AddressSpec = do
   describe "IPv4 address" $ do
     it "Reading" $ do
-      show (read "192.168.0.255" :: Ipv4Address) `shouldBe` "192.168.0.255"
+      show (read "192.168.0.255" :: Address) `shouldBe` "192.168.0.255"
 
 ipv6AddressSpec :: Spec
 ipv6AddressSpec = do
   describe "IPv6 address" $ do
     it "Reading" $ do
-      show (read "::"            :: Ipv6Address)
-        `shouldBe` "0:0:0:0:0:0:0:0"
-      show (read "2001:db8::"    :: Ipv6Address)
-        `shouldBe` "2001:db8:0:0:0:0:0:0"
-      show (read "::ffff:0:0"    :: Ipv6Address)
-        `shouldBe` "0:0:0:0:0:ffff:0:0"
-      show (read "2001:db8::2:1" :: Ipv6Address)
-        `shouldBe` "2001:db8:0:0:0:0:2:1"
+      show (read "::" :: Address) `shouldBe` "::"
+      show (read "2001:db8::" :: Address) `shouldBe` "2001:db8::"
+      show (read "::ffff:0:0" :: Address) `shouldBe` "::ffff:0:0"
+      show (read "2001:db8:0:0:0:0:2:1" :: Address) `shouldBe` "2001:db8::2:1"
+      show (read "2001:db8:0:1:1:1:1:1" :: Address)
+        `shouldBe` "2001:db8:0:1:1:1:1:1"
+      show (read "2001:0:0:1:0:0:0:1" :: Address) `shouldBe` "2001:0:0:1::1"
+      show (read "2001:db8:0:0:1:0:0:1" :: Address)
+        `shouldBe` "2001:db8::1:0:0:1"
