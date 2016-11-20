a463c3a8960069817f1ef088f369393afb20b850
vonavi <ivvl82@gmail.com>
2016-11-20 17:29:42 +0300

    Data.Zipper: add Haddock comments.

--- src/Data/Zipper.hs
+++ src/Data/Zipper.hs
@@ -1,15 +1,17 @@
+-- | Traverses a tree of memory pages by zipper.
 module Data.Zipper
   (
     Zipper(..)
   ) where
 
+-- | Zipper typeclass
 class Zipper a where
-  goLeft   :: a -> a
-  goRight  :: a -> a
-  goUp     :: a -> a
-  isLeaf   :: a -> Bool
-  getLabel :: a -> Maybe Int
-  setLabel :: Maybe Int -> a -> a
-  size     :: a -> Int
-  insert   :: a -> a -> a
-  delete   :: a -> a
+  goLeft   :: a -> a              -- ^ Goes left.
+  goRight  :: a -> a              -- ^ Goes right.
+  goUp     :: a -> a              -- ^ Goes up.
+  isLeaf   :: a -> Bool           -- ^ Is a leaf reached or not.
+  getLabel :: a -> Maybe Int      -- ^ Gets label.
+  setLabel :: Maybe Int -> a -> a -- ^ Sets label.
+  size     :: a -> Int            -- ^ Gets the size of page.
+  insert   :: a -> a -> a         -- ^ Inserts a page here.
+  delete   :: a -> a              -- ^ Deletes page here.
